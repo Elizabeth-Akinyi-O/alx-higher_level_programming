5bd77ab61b685430c37e2c8fe338866e7391349a
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2024-01-03 11:45:54 +0300

    Prints a string in uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in str: #i is the iterator
-        temp = i #temp is the temporary file
+    for i in str:
+        temp = i
         if ord(temp) >= 97 and ord(temp) <= 122:
             temp = chr(ord(i) - 32)
         print("{}".format(temp), end='')
