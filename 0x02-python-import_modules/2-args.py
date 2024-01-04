df21e08c124637b4917408a7dc23987d407d8297
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2024-01-04 22:57:12 +0300

    Corrections on No.1 && No.2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,10 @@ if __name__ == "__main__":
     from sys import argv
     a = len(argv) - 1
     if  a < 1:
-        print("{} arguments:".format(a))
+        print("{} arguments.".format(a))
     elif a == 1:
         print("{} argument:".format(a))
     else:
         print("{} arguments:".format(a))
-
     for b in range(a):
         print("{}: {:s}".format(b + 1, argv[b + 1]))
