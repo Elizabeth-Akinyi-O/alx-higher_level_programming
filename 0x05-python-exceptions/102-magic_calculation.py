a509eff6f1efaa1e3401b75b7f9b4b5eef9bf6c6
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2024-01-22 09:04:46 +0300

    ByteCode -> Python #4

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
-    for x in range(1, 3)
+    for x in range(1, 3):
         try:
             if x > a:
                 raise exception('Too far')
