8fd208c3305f221bc9d4c67e40d4c26512482b7f
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2024-03-13 02:59:21 +0300

    Number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,7 @@
--- lists the number of records with the same score in
---    the table second_table of the database hbtn_0c_0
---      in your MySQL server
+-- Lists the number of records with the same score in
+--    the table second_table
+-- Displays the number of records for this score with
+--    the label number
 
 SELECT `score`, COUNT(*) AS `number`
 FROM `second_table`
