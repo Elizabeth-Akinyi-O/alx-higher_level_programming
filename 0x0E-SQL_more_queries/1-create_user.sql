9469d3a1be6b663127c7dd170bb8eeb6013dcf69
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2024-03-14 12:51:42 +0300

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,11 +4,7 @@
 --   If the user user_0d_1 already exists, script should not fail
 
 
-CREATE USER
-IF NOT EXISTS user_0d_1@localhost
-IDENTIFIED BY user_0d_1_pwd;
+CREATE USER IF NOT EXISTS `user_0d_1@localhost` IDENTIFIED BY `user_0d_1_pwd`;
 FLUSH PRIVILEGES;
-GRANT ALL PRIVILEGES
-ON *.*
-TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES ON *.* TO `user_0d_1@localhost`;
 FLUSH PRIVILEGES;
