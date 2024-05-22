92e2e6d18177f0c8247f84a9cac9bc369f53be88
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2024-05-23 00:05:36 +0300

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// display the status code of a GET request
+// The first argument is the URL to request (GET)
+// The status code must be printed like this: code: <status code>
+
+const request = require('request');
+const url = process.argv[2];
+request.get(url, function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:' + ' ' + response.statusCode);
+  }
+});
