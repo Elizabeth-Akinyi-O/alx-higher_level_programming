a85bcfead336e41ee9277c53712104bd0d5fb419
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2024-05-26 12:08:10 +0300

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,25 +5,33 @@
 // You must use the Star wars API
 
 const request = require('request');
-const movieId = process.argv[2];
-const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request.get(movieUrl, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const movie = JSON.parse(body);
-    const characters = movie.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
+const MyFiles = process.argv.slice(2);
+let data;
+let i;
+let MyList = [];
+function GetCharId (id) {
+  request('https://swapi-api.hbtn.io/api/films/' + id,
+    async function (error, response, body) {
+      if (error) console.error(error);
+      else {
+        data = JSON.parse(body);
+        MyList = data.characters;
+        for (i = 0; i < MyList.length; i++) {
+          const name = await GetName(MyList[i]);
+          console.log(name);
         }
-      });
-    }
-  }
-});
+      }
+    });
+}
+
+function GetName (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) reject(error);
+      else {
+        resolve(JSON.parse(body).name);
+      }
+    });
+  });
+}
+GetCharId(MyFiles[0]);
